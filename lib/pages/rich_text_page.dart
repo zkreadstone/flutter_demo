f995cc042c8e272e2e78a395a572dd00e7c77abe
zkreadstone <zhangkangreadstone@163.com>
2021-02-19 16:45:38 +0800

    微调展示UI

--- lib/pages/rich_text_page.dart
+++ lib/pages/rich_text_page.dart
@@ -14,24 +14,31 @@ class _RichTextPageState extends State<RichTextPage> {
     return Scaffold(
       body:Container(
         width: MediaQuery.of(context).size.width,height: MediaQuery.of(context).size.height,
-        padding: EdgeInsets.only(top:120),
-        child: Column(
-          children:[
+        padding: EdgeInsets.only(top:120,left: 20,right:20),
+        child: ListView(
+            children:[
             MarkDownContentView(
               content: '*斜体*',
             ),
+            SizedBox(height: 20,),
+
              MarkDownContentView(
               content: '**加粗**',
             ),
+            SizedBox(height: 20,),
+            
              MarkDownContentView(
               content: '__下划线__',
             ),
+            SizedBox(height: 20,),
              MarkDownContentView(
               content: '||隐藏||',
             ),
+            SizedBox(height: 20,),
              MarkDownContentView(
               content: '`代码段`',
             ),
+            SizedBox(height: 20,),
              MarkDownContentView(
               content: 'http://www.baidu.com',
             ),
